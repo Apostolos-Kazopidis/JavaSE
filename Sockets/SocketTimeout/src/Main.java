2de30527c6242d03024e5fb2155c96bd0f88f216
Flazeras <flazerakis@gmail.com>
2023-08-02 17:54:28 +0300

    JavaSE

---
+++ Sockets/SocketTimeout/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        try (Socket s = new Socket()) {
+            s.connect(new InetSocketAddress("www.psounis.gr", 442), 1000);
+            System.out.println("Status of connection: " + s.isConnected());
+        } catch (SocketTimeoutException e) {
+            System.out.println("Connection time expired!");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
