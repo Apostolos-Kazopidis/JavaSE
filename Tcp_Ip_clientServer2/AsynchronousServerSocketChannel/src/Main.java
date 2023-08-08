d52fc1e7dee37a5e40f165bdea90257f1a85ed87
Flazeras <flazerakis@gmail.com>
2023-08-08 15:12:00 +0300

    JavaSE

---
+++ Tcp_Ip_clientServer2/AsynchronousServerSocketChannel/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+import java.nio.channels.*;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // make the server
+        try (AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open()) {
+            server.bind(new InetSocketAddress(1234));
+            System.out.println("Server ready!");
+
+            Future<AsynchronousSocketChannel> futureClient = server.accept();
+            for (int i = 0; i < 5; i++) {
+                if (!futureClient.isDone()) {
+                    System.out.println("no incoming connections");
+                    Thread.sleep(500);
+                } else {
+                    AsynchronousSocketChannel sockClient = futureClient.get();
+                }
+
+            }
+        } catch (IOException | InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Tcp_Ip_clientServer2/SelectorChannelServer/server/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // make the selector and the server
+        try (Selector selector = Selector.open();
+             ServerSocketChannel server = ServerSocketChannel.open()) {
+
+            server.configureBlocking(false); // important! Selectors only use non-blocking channels
+            server.bind(new InetSocketAddress(1234));
+            server.register(selector, SelectionKey.OP_ACCEPT);
+
+            while (true) {
+                Thread.sleep(1000);
+                selector.selectNow();
+                Set<SelectionKey> keyEvents = selector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = keyEvents.iterator();
+                System.out.print("\nEvents: ");
+                while (keyIterator.hasNext()) {
+                    SelectionKey keyEvent = keyIterator.next();
+                    server.accept();
+                    System.out.print(keyEvent + " ");
+                    keyIterator.remove();
+                }
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
